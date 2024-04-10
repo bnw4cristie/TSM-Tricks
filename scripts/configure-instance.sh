ad21c7a2ed60297711296aade21a92444494e1c5
Bjørn Nachtwey <143603270+bnw4cristie@users.noreply.github.com>
2024-04-10 11:25:26 +0200

    Update configure-instance.sh
    first commands

--- scripts/configure-instance.sh
+++ scripts/configure-instance.sh
@@ -1,3 +1,21 @@
 #! /bin/bash
 
-echo "T.B.D. :-("
+# check for beeing root
+if [[ $USER != "root" ]]
+then
+  echo "Must be root to execute this script! -- Aborting"
+  exit
+fi
+
+# create TSMDB1 
+echo "creating database ..."
+/opt/tivoli/tsm/db2/instance/db2icrt -a server -u tsm02 tsm02
+
+if [[ $? ]]
+then
+  echo " ... successful :-)"
+else
+  echo " ... failed :-("
+  echo "please issue ..."
+  exit
+fi
